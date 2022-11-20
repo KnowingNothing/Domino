31a4eecf9637bc98b0c036337757440a5904a15e
KnowingNothing <zhengsizeMax@outlook.com>
2022-11-20 12:50:04 +0800

    feat(acc): add gemm nvdla

--- python/domino/accelerator/nvdla.py
+++ python/domino/accelerator/nvdla.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from ..base import AcceleratorBase, AccTask
 from ..utils import run_maestro, generate_maestro_command, find_maestro
 from .conv_acc import ConvAccelerator
+from .gemm_acc import GemmAccelerator
 
 
 class NVDLA(ConvAccelerator):
@@ -53,3 +54,44 @@ class NVDLA(ConvAccelerator):
 
     def __repr__(self) -> str:
         return f'NVDLA{self.topo_id}'
+
+
+class GemmNVDLA(GemmAccelerator):
+    def __init__(self, name, n_stream=1, freq=200, num_pes=128*128, noc_bw=81920000, off_chip_bw=81920000, l1_size=4000000, l2_size=24000000) -> None:
+        super(GemmNVDLA, self).__init__(name, n_stream, freq=freq, num_pes=num_pes, noc_bw=noc_bw,
+                                        off_chip_bw=off_chip_bw, l1_size=l1_size, l2_size=l2_size)
+
+    def get_mapping(self, M, N, K):
+        # N * 64
+        mapping = ("Network sample_net {\n"
+                   "Layer GEMM {\n"
+                   "Type: GEMM\n"
+                   "Dimensions { "
+                   f"K: {K}, M: {M}, N: {N} "
+                   "}\n"
+                   "Dataflow {\n"
+                   "        SpatialMap(1, 1) N;\n"
+                   "        TemporalMap(64, 64) K;\n"
+                   "        TemporalMap(1, 1) M;\n"
+                   "        Cluster(64, P);\n"
+                   "        SpatialMap(1, 1) K\n"
+                   "        TemporalMap(1, 1) M;\n"
+                   "        TemporalMap(1, 1) N;\n"
+                   "}\n"
+                   "}\n"
+                   "}\n")
+
+        return mapping
+
+    def spatial_used_pes(self, M, N, K):
+        """
+        Return how many PEs are actually needed
+        This is calculated according to the mapping
+        """
+        return min(N * 64, self.num_pes)
+
+    def __str__(self) -> str:
+        return f'GemmNVDLA{self.topo_id}'
+
+    def __repr__(self) -> str:
+        return f'GemmNVDLA{self.topo_id}'
