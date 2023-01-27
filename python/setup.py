fe4147b3ff9ed03923fed4682754a76a7d30c62c
Light-of-Hers <724610746@qq.com>
2023-01-27 22:49:07 +0800

    rfact(setup): add mculib setup

--- python/setup.py
+++ python/setup.py
@@ -6,11 +6,16 @@ setup(
     packages=find_packages(),
 )
 
-
 os.chdir("../build")
 setup(
     name="dominoc",
     packages=[""],
     package_dir={"": "."},
     package_data={"": ["dominoc*.so"]},
+)
+
+os.chdir("../testing/mculib/python")
+setup(
+    name="mculib",
+    packages=find_packages(),
 )
